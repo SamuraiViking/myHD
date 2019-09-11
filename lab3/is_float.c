2f68f88899cd257d87f49202cdba0237ee13818a
Kevin <nelson67@stolaf.edu>
2019-09-11 12:17:26 -0500

    done print is int

--- lab3/is_float.c
+++ lab3/is_float.c
@@ -5,19 +5,28 @@
 
 float printSign(float num) {
   if(num > 0) {
-    printf("%f is positive", num);
+    printf("%f is positive\n", num);
   } else if(num == 0) {
-    printf("%f is zero", num);
+    printf("%f is zero\n", num);
   } else {
-    printf("%f is negative", num);
+    printf("%f is negative\n", num);
   }
   return num;
+}
+
+bool isInt(float num) {
+  return (int)num == num;
+}
+
+void printIsInt(float num) {
+  printf(isInt(num) ? "%f is an integer\n" : "%f is not an integer\n", num);
 }
 
 
 int main() {
 
   printSign(7.3);
+  printIsInt(10.00);
 
   return 0;
 
