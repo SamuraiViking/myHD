c059ad1bd2863f31ed22ef1d76debbc5a1124990
Kevin <nelson67@stolaf.edu>
2019-09-11 12:40:01 -0500

    make exe file

---
+++ lab3/test2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool floatIsInt(float num) {
+  return (int)num == num;
+}
+
+float printNumberFacts(float num) {
+  if(num > 0) {
+    if(floatIsInt(num)) {
+      printf("%f is an integer\n", num);
+      if(floatIsInt(sqrt(num))) {
+        printf("%f's square, %f is an integer\n", num, sqrt(num));
+      }
+    }
+    printf("%f is positive\n", num);
+  } else if(num == 0) {
+    printf("%f is zero\n", num);
+  } else {
+    printf("%f is negative\n", num);
+  }
+  return num;
+}
+
+
+
+int main() {
+
+  printf("### TESTING ###\n\n");
+  printf("--- 25 ---\n");
+  printNumberFacts(25.000);
+  printf("--- 17 ---\n");
+  printNumberFacts(17.000);
+  printf("--- 15 ---\n");
+  printNumberFacts(15.000);
+
+  return 0;
+
+}
+
