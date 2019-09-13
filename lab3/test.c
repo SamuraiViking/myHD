36532f1480fa7a500fc5ec44a79a690c91ea05e6
Kevin <nelson67@stolaf.edu>
2019-09-13 11:27:13 -0500

    lab4: part A step 1a only

--- lab3/test.c
+++ lab3/test.c
@@ -9,14 +9,14 @@ bool floatIsInt(float num) {
 
 float printNumberFacts(float num) {
   if(num > 0) {
-    if(floatIsInt(num)) {
+    if (floatIsInt(num)) {
       printf("%f is an integer\n", num);
-      if(floatIsInt(sqrt(num))) {
+      if (floatIsInt(sqrt(num))) {
         printf("%f's square, %f is an integer\n", num, sqrt(num));
       }
     }
     printf("%f is positive\n", num);
-  } else if(num == 0) {
+  } else if (num == 0) {
     printf("%f is zero\n", num);
   } else {
     printf("%f is negative\n", num);
--- lab4/example.c
+++ lab4/example.c
@@ -8,15 +8,15 @@ bool floatIsInt(float num) {
 }
 
 float printNumberFacts(float num) {
-  if (num > 0) {
-    if (floatIsInt(num)) {
+  if(num > 0) {
+    if(floatIsInt(num)) {
       printf("%f is an integer\n", num);
-      if (floatIsInt(sqrt(num))) {
+      if(floatIsInt(sqrt(num))) {
         printf("%f's square, %f is an integer\n", num, sqrt(num));
       }
     }
     printf("%f is positive\n", num);
-  } else if (num == 0) {
+  } else if(num == 0) {
     printf("%f is zero\n", num);
   } else {
     printf("%f is negative\n", num);
@@ -28,14 +28,11 @@ float printNumberFacts(float num) {
 
 int main() {
 
-  printf("### TESTING ###\n\n");
-  printf("--- 25 ---\n");
-  printNumberFacts(25.000);
-  printf("--- 17 ---\n");
-  printNumberFacts(17.000);
-  printf("--- 15 ---\n");
-  printNumberFacts(15.000);
-
+  float input;
+  puts("Give me a number:");
+  scanf("%f", &input);
+  printf("\n");
+  printNumberFacts(input);
   return 0;
 
 }
