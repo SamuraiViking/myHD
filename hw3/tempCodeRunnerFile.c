b540383505caa39a79157f183e79189d86a6881a
Kevin <nelson67@stolaf.edu>
2019-09-25 11:27:34 -0500

    lab A7: complete

---
+++ hw3/tempCodeRunnerFile.c
@@ -0,0 +1,2 @@
+  // printf("Enter some values, pres CTRL/D when your done\n");
+  // printf("The return value from calling mean_input() is %f\n", mean_input());
---
+++ lab7/tryhex.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main() {
+  int input = 0xc93e;
+  printf("The value of x is %d in decimal, %x in hexadecimal\n",input, input);
+}
